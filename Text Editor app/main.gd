310a1fb41f2b6c24d565542e03083a402c66c8e7
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2023-12-06 21:01:01 -0800

    Update comments main.gd
    Adds comments that explain the code

--- Text Editor app/main.gd
+++ Text Editor app/main.gd
@@ -1,24 +1,21 @@
 extends Control
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
+# event called when the open button is pressed by the user its connected by a signal which can be set with the Node tap in the editor
 func _on_open_pressed():
-	$FileDialog.popup()
+	$FileDialog.popup() #this unhides the node using the popup method
 
 func _n_save_pressed():
-	$SaveFileDialog2.popup()
+	$SaveFileDialog2.popup() #this unhides the node using the popup method
 
 func _on_file_dialog_file_selected(path):
-	print(path)
-	var file1 = FileAccess.open(path, FileAccess.READ) # Use FileAccess.open instead of file2.new
-	$TextEdit.text = file1.get_as_text() # Use $TextEdit.text instead of $TextEdit.txt
-	file1.close() # Close the file after reading
+	print(path) # prints the path selected by the user
+	var file1 = FileAccess.open(path, FileAccess.READ) # Opens the file at the given path in read mode and returns a FileAccess object
+	$TextEdit.text = file1.get_as_text() # Sets the text property of the TextEdit node to the content of the file as a string
+	file1.close() # Closes the file after reading
 
 func _on_save_file_dialog_2_file_selected(path):
-	print(path)
-	var file1 = FileAccess.open(path, FileAccess.WRITE) # Use FileAccess.open instead of file2.new
-	file1.store_string($TextEdit.text) # Use file1.store_string to write the text to the file
-	file1.close() # Close the file after writing
+	print(path) # prints the path selected for debug
+	var file1 = FileAccess.open(path, FileAccess.WRITE) # Opens the file at the given path in write mode and returns a FileAccess object
+	file1.store_string($TextEdit.text) # Stores the text property of the TextEdit node to the file as a string
+	file1.close() # Closes the file after writing
 
